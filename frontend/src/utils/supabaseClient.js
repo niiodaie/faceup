a99c335249b8b06bfc02fa2922697b3d8d4684ec
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-26 06:34:38 -0500

    Update supabaseClient.js

--- frontend/src/utils/supabaseClient.js
+++ frontend/src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = https://rniewznrqnifzzdnvuct.supabase.co;
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuaWV3em5ycW5pZnp6ZG52dWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0MzE2NDMsImV4cCI6MjA2OTAwNzY0M30.WFk1MX-awkNVR84--ywHdKJLVBiKNsxQMWzc44lPmw8;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
