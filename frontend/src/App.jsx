67c7124416908fcc4f874f621525cb7168d154b1
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-26 22:42:12 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router, Navigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
