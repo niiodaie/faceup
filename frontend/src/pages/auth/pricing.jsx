5346b305469a52e9c0307604258f94341365685a
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-12-10 20:50:33 -0600

    Create pricing.jsx

---
+++ frontend/src/pages/auth/pricing.jsx
@@ -0,0 +1,9 @@
+import CheckoutCard from "../components/CheckoutCard";
+
+export default function PricingPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 pt-20">
+      <CheckoutCard />
+    </div>
+  );
+}
