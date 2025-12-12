378704aec238becbcaacaf75d15a86fb4b69324c
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-12-12 06:51:29 -0600

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import multer from 'multer';
-import { handleStripeWebhook } from './webhooks/stripeWebhook.js';
+
 
 
 // Core handlers
